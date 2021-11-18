adc86104b66ee7c64d178a5b00dc4f8eb25fa088
teghfo <teghfo@gmail.com>
2021-11-18 19:08:31 +0330

    add entity models

--- OOP/library_management/models.py
+++ OOP/library_management/models.py
@@ -1,9 +1,10 @@
-# serialize data python
-
+import settings
+import pickle
 
 #### Users
 class BaseUser:
     type = None
+    file_name = None
 
     def __init__(self, username, password) -> None:
         self.username = username
@@ -14,29 +15,35 @@ class BaseUser:
     def _generate_id():
         pass
 
+
     @staticmethod
     def _set_password(passw):
         pass
     
 
     def save(self):
-        pass
+        try:
+            with open(settings.USER_DATA_PATH / self.file_name, "ab") as db_file:
+                pickle.dump(self, db_file)
+
+        except Exception as e:
+            print(e)
+
 
 class Admin(BaseUser):
     type = "Admin"
+    file_name = "admins.db"
 
     def add_user(self):
         pass
 
-    def save(self):
-        
-
 
 class Member(BaseUser):
     type = "Member"
+    file_name = "members.db"
+
 
 
 
 
 ### Books
-class 
