6fea3aa19009e491dddd4daab31470a7b8456506
teghfo <teghfo@gmail.com>
2021-11-18 19:10:23 +0330

    add settings

--- OOP/library_management/settings.py
+++ OOP/library_management/settings.py
@@ -5,3 +5,9 @@ from pathlib import Path
 print(Path(__file__).resolve().parent.parent)
 
 BASE_DIR = Path(__file__).resolve().parent
+
+USER_DATA_PATH = BASE_DIR / "users_data"
+BOOKS_DATA_PATH = BASE_DIR / "books_data"
+
+
+
