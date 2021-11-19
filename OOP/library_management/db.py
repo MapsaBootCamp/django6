3186d7e570c1d05e554135aa3ab2405703b4e2af
teghfo <teghfo@gmail.com>
2021-11-19 11:49:06 +0330

    complete query

--- OOP/library_management/db.py
+++ OOP/library_management/db.py
@@ -2,8 +2,12 @@ import pickle
 
 class BaseQuery:
 
-    def loadall(self, filename):
-        with open(filename, "rb") as f:
+
+    def __init__(self, file_path) -> None:
+        self.file_path = file_path
+
+    def loadall(self):
+        with open(self.file_path, "rb") as f:
             while True:
                 try:
                     yield pickle.load(f)
@@ -19,8 +23,10 @@ class BaseQuery:
         pass
 
 
+    def exist(self, query_params, query_value) -> bool:
+        for elm in self.loadall():
+            if getattr(elm, query_params) == query_value:
+                return True
+        return False
 
 
-
-class QueryUsers(BaseQuery):
-    pass
--- OOP/library_management/models.py
+++ OOP/library_management/models.py
@@ -1,10 +1,22 @@
 import settings
 import pickle
+from uuid import uuid4
+
+from db import BaseQuery
+
+class MetaBase(type):
+    
+    def __init__(self, *args, **kwargs):
+        self.query = BaseQuery(self.file_path)
+        super().__init__(*args, **kwargs)
+
 
 #### Users
-class BaseUser:
+class BaseUser(metaclass=MetaBase):
     type = None
     file_name = None
+    file_path = None
+
 
     def __init__(self, username, password) -> None:
         self.username = username
@@ -13,12 +25,12 @@ class BaseUser:
 
     @staticmethod
     def _generate_id():
-        pass
+        return str(uuid4())
 
 
     @staticmethod
     def _set_password(passw):
-        pass
+        return hash(passw)
     
 
     def save(self):
@@ -33,6 +45,7 @@ class BaseUser:
 class Admin(BaseUser):
     type = "Admin"
     file_name = "admins.db"
+    file_path = settings.USER_DATA_PATH / file_name
 
     def add_user(self):
         pass
@@ -41,9 +54,36 @@ class Admin(BaseUser):
 class Member(BaseUser):
     type = "Member"
     file_name = "members.db"
-
-
+    file_path = settings.USER_DATA_PATH / file_name
 
 
 
 ### Books
+class BaseBook(metaclass=MetaBase):
+    file_name = None
+    file_path = None
+
+
+    def __init__(self, name, author) -> None:
+        self.name = name
+        self.author = author
+        self.id = self._generate_id()
+
+    @staticmethod
+    def _generate_id():
+        return str(uuid4())
+
+
+    def save(self):
+        try:
+            with open(settings.BOOKS_DATA_PATH / self.file_name, "ab") as db_file:
+                pickle.dump(self, db_file)
+
+        except Exception as e:
+            print(e)
+
+
+class Book(BaseBook):
+    file_name = "books.db"
+    file_path = settings.BOOKS_DATA_PATH / file_name
+
