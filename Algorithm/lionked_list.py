e15ad40a8461300535419e2d35e42c986e67c9d8
teghfo <teghfo@gmail.com>
2021-11-25 16:06:25 +0330

    linked list

---
+++ Algorithm/lionked_list.py
@@ -0,0 +1,86 @@
+from typing import Optional
+
+
+class Element:
+
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+        self.free = True
+
+    def add_to_linked_list(self):
+        self.free = False
+
+    def free_from_linked_list(self):
+        self.free = True
+        self.next = None
+
+    def is_free(self):
+        if self.free:
+            return True
+        else:
+            return False
+
+class LinkedList:
+
+    def __init__(self) -> None:
+        self.root: Optional[Element] = None
+
+    def add_begin(self, node: Element) -> None:
+        if not node.is_free():
+            raise Exception("elemebt must be free!")
+
+        if self.root is None:
+            self.root = node
+        else:
+            node.next = self.root
+            self.root = node
+            node.add_to_linked_list()
+
+
+    def __len__(self):
+        pass
+
+    def push(self, node: Element) -> None:
+        pass
+
+    def pop(self) -> None:
+        pass
+
+    def search(self, node: Element) -> bool:
+        pass
+
+    def add_between(self, node1: Element, node2: Element):
+        pass
+
+    def remove(self, node: Element):
+        pass
+
+    def __str__(self) -> str:
+        result = "["
+        if self.root is None:
+            result += "]"
+            return result
+        else:
+            result += str(self.root.data)
+            temp = self.root
+            while temp.next:
+                result += "-> "
+                temp = temp.next
+                result += str(temp.data)
+            result += "]"
+
+        return result
+
+
+elm1 = Element(2)
+elm2 = Element("salam")
+elm3 = Element([1, 2, 3])
+        
+
+l1 = LinkedList()
+l1.add_begin(elm1)
+l1.add_begin(elm2)
+l1.add_begin(elm3)
+
+print(l1)
