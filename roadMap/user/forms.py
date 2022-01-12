c50589e350043edf2b3699903fc6812a9920c8b9
BAD <fathibehrad@gmail.com>
2022-01-12 17:17:02 +0330

    task completed

--- roadMap/user/forms.py
+++ roadMap/user/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 
+from .models import Mentor
+
 User = get_user_model()
 
 
@@ -11,6 +13,43 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ['email',
                   'password1', 'password2']
+        labels = {
+            "email": "ایمیل",
+            "password1": "رمز عبور",
+            "password2": "تکرار رمز عبور",
+
+        }
+
+        help_texts = {
+            "email": "ایمیل خود را به درستی وارد کنید",
+        }
+
+    def save(self, commit=True):
+        '''
+        override user create form to create profile after register!
+        '''
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.save()
+
+        # try:
+        #     # if commit:
+        #     user.save()
+        #     Profile.objects.create(user=user, phone=self.cleaned_data["phone"])
+
+        # except Exception as e:
+        #     user.delete()
+        #     raise ValueError(f"cant create profile object! reason: {e}")
+
+        return user
+
+
+class CustomMentorCreationForm(UserCreationForm):
+
+    class Meta:
+        model = Mentor
+        fields = ['email',
+                  'password1', 'password2', 'major', 'user_permissions']
         labels = {
             "email": "ایمیل",
             "password1": "رمز عبور",
