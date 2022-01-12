38bcaf95d2359e6c1e18ee68309d6ab3a76b866b
mahdi <syadtym983@gmail.comm>
2022-01-12 15:41:27 +0330

    mahdi_task

--- roadMap/courses/admin.py
+++ roadMap/courses/admin.py
@@ -5,8 +5,18 @@ from .models import CourseCategory, Course, Comment
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    pass
 
+    # def get_queryset(self, request):
+    #     qs = super().get_queryset(request)
+    #     return qs.filter(mentor=request.user)
+
+     def get_queryset(self, request, *args, **kwargs):
+        if request.user.is_superuser:
+            return super().get_queryset(request, *args, **kwargs)
+
+        elif request.user.has_perm("user.Mentor_access"):
+            qs_main = Course.objects.filter(mentor__id=request.user.id)
+            return qs_main
 
 @admin.register(CourseCategory)
 class CourseCategoryAdmin(admin.ModelAdmin):
